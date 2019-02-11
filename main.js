63c3897045edd8708699d0d73b5cefd86ad8b6d6
DAniel <dagr99002@utb.vaxjo>
2019-02-11 13:37:32 +0100

    functioning auth & route recording

--- main.js
+++ main.js
@@ -1,34 +1,71 @@
 function initMap() {
-	if (Modernizr.geolocation) {
-		navigator.geolocation.getCurrentPosition(loadMap)
-	}
+    if (Modernizr.geolocation) {
+        navigator.geolocation.getCurrentPosition(loadMap)
+    }
 }
 
 
 var map
 var poly
 
+var provider = new firebase.auth.GoogleAuthProvider()
+
 var data = {
-	sender: null,
-	timestamp: null,
-	lat: null,
-	lng: null
+    sender: null,
+    timestamp: null,
+    lat: null,
+    lng: null
 }
 
 /**
  * Starting point for running the program. Authenticates the user.
  * @param {function} Called when authentication succeeds.
  */
-function initAuthentication(onAuthSuccess) {
-	firebase.authAnonymously(function (error, authData) {
-		if (error) {
-			console.log('Login Failed!', error);
-		} else {
-			data.sender = authData.uid;
-			onAuthSuccess();
-		}
-	}, { remember: 'sessionOnly' });  // Users will get a new id for every session.
+function initAuthentication() {
+
+    firebase.auth().signInWithPopup(provider).then(result => {
+        var token = result.credential.accessToken
+        console.log('token: ' + token)
+
+        var user = result.user.email
+        console.log('user: ' + user)
+
+    }).catch(error => {
+        var errorCode = error.code
+        var errorMessage = error.message
+        var email = error.email
+        var credential = error.credential
+    })
+
 }
+
+function toggleAuthentication() {
+    let signedIn = !!firebase.auth().currentUser
+
+    if (signedIn) {
+        firebase.auth().signOut()
+    } else {
+        initAuthentication()
+    }
+}
+
+
+
+window.addEventListener('load', () => {
+    console.log("window.addEventListener load")
+    firebase.auth().onAuthStateChanged(u => {
+        console.log("onUseruChagneru", u)
+
+        let signedIn = u != null;
+
+        let signInButton = document.getElementById('sign-in-btn')
+        if (signedIn) {
+            signInButton.innerText = "Sign out"
+        } else {
+            signInButton.innerText = "Sign in"
+        }
+    })
+});
 
 /**
  * Adds a click to firebase.
@@ -40,143 +77,217 @@ function initAuthentication(onAuthSuccess) {
 
 
 function loadMap(position) {
-	latitude = position.coords.latitude
-	longitude = position.coords.longitude
+    latitude = position.coords.latitude
+    longitude = position.coords.longitude
 
-	console.log(latitude + " : " + longitude)
+    console.log(latitude + " : " + longitude)
 
-	var dasPos = { lat: latitude, lng: longitude }
-	console.log(dasPos);
+    var dasPos = { lat: latitude, lng: longitude }
+    console.log(dasPos);
 
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: dasPos,
-		zoom: 17,
-		gestureHandling: 'greedy',
-		styles: mapStyles
-	})
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: dasPos,
+        zoom: 17,
+        gestureHandling: 'greedy',
+        styles: mapStyles
+    })
 
 
-	poly = new google.maps.Polyline({
-		strokeColor: '#FA0909',
-		strokeOpacity: 0.95,
-		strokeWeight: 1
-	})
-	poly.setMap(map)
-	map.addListener('click', addLatLng)
+    poly = new google.maps.Polyline({
+        strokeColor: '#FA0909',
+        strokeOpacity: 0.95,
+        strokeWeight: 1
+    })
+    poly.setMap(map)
+    map.addListener('click', addLatLng)
 
-    var adaRef = firebase.database().ref('routes')
-	console.log(adaRef);
-	
-	//setInterval(() => {
-		myLatLng = new google.maps.LatLng({ lat: position.coords.latitude, lng: position.coords.longitude })
+    console.log(adaRef);
 
-		adaRef.push({latitude: position.coords.latitude, longitude: position.coords.longitude})
-		var path = poly.getPath()
-		path.push(myLatLng)
-		console.log(myLatLng);
+    //setInterval(() => {
+    myLatLng = new google.maps.LatLng({ lat: position.coords.latitude, lng: position.coords.longitude })
 
-		var marker = new google.maps.Marker({
-			position: myLatLng,
-			title: '#' + path.getLength(),
-			animation: google.maps.Animation.BOUNCE,
-			map: map
-		})
-	//}, 5000)
+    var path = poly.getPath()
+    path.push(myLatLng)
+    console.log(myLatLng);
 
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        title: '#' + path.getLength(),
+        animation: google.maps.Animation.BOUNCE,
+        map: map
+    })
+    //}, 5000)
+
+}
+
+function listenForRoutes() {
+    firebase.database().ref('routes').on('value', snapshot => {
+        let routes = snapshot.val();
+
+        console.log("routes", routes);
+
+
+        var megaPolyLine = new google.maps.Polyline({
+            strokeColor: '#FA0909',
+            strokeOpacity: 0.95,
+            strokeWeight: 1
+        });
+        megaPolyLine.setMap(map)
+        let path = megaPolyLine.getPath()
+
+        for (var routeId in routes) {
+            let coordinates = routes[routeId].coordinates;
+
+            for (var coordinate of coordinates) {
+                path.push(new google.maps.LatLng(coordinate.latitude, coordinate.longitude))
+            }
+        }
+    })
+}
+
+/**
+ * Array of coordinates of the currently recorded route.
+ */
+var routeRecording = [];
+var isRecordingRoute = false;
+function toggleRecordRoute() {
+    if (isRecordingRoute) {
+        stopRecordRoute();
+    } else {
+        startRecordRoute();
+    }
+}
+
+function startRecordRoute() {
+    document.getElementById('record-route-btn').innerText = "Stop recording";
+    routeRecording = []
+    isRecordingRoute = true;
+}
+
+function stopRecordRoute() {
+    document.getElementById('record-route-btn').innerText = "Start recording";
+
+    pushRecordedRoute();
+    routeRecording = []
+    isRecordingRoute = false;
+}
+
+/**
+ * Pushes the recorded route to firebase.
+ */
+function pushRecordedRoute() {
+    let name = prompt("Vad ska vi kalla stigen?");
+
+    firebase.database().ref('routes').push({
+        coordinates: routeRecording,
+        name
+    })
 }
 
 function addLatLng(e) {
-	var path = poly.getPath()
-	path.push(e.latLng)
-	console.log(e.latLng);
+    let latLng = e.latLng;
+    let latitude = latLng.lat();
+    let longitude = latLng.lng();
 
-	var marker = new google.maps.Marker({
-		position: e.latLng,
-		title: '#' + path.getLength(),
-		animation: google.maps.Animation.BOUNCE,
-		map: map
-	})
+    var path = poly.getPath()
+    path.push(latLng)
+    console.log(latLng);
+
+    if (isRecordingRoute) {
+        routeRecording.push({
+            latitude,
+            longitude
+        })
+    }
+
+    /*var marker = new google.maps.Marker({
+        position: e.latLng,
+        title: '#' + path.getLength(),
+        animation: google.maps.Animation.BOUNCE,
+        map: map
+    })*/
 }
 
+window.addEventListener('load', listenForRoutes);
 
 
 var mapStyles = [
-	{ elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
-	{ elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
-	{ elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
-	{
-		featureType: 'administrative.locality',
-		elementType: 'labels.text.fill',
-		stylers: [{ color: '#d59563' }]
-	},
-	{
-		featureType: 'poi',
-		elementType: 'labels.text.fill',
-		stylers: [{ color: '#d59563' }]
-	},
-	{
-		featureType: 'poi.park',
-		elementType: 'geometry',
-		stylers: [{ color: '#263c3f' }]
-	},
-	{
-		featureType: 'poi.park',
-		elementType: 'labels.text.fill',
-		stylers: [{ color: '#6b9a76' }]
-	},
-	{
-		featureType: 'road',
-		elementType: 'geometry',
-		stylers: [{ color: '#38414e' }]
-	},
-	{
-		featureType: 'road',
-		elementType: 'geometry.stroke',
-		stylers: [{ color: '#212a37' }]
-	},
-	{
-		featureType: 'road',
-		elementType: 'labels.text.fill',
-		stylers: [{ color: '#9ca5b3' }]
-	},
-	{
-		featureType: 'road.highway',
-		elementType: 'geometry',
-		stylers: [{ color: '#746855' }]
-	},
-	{
-		featureType: 'road.highway',
-		elementType: 'geometry.stroke',
-		stylers: [{ color: '#1f2835' }]
-	},
-	{
-		featureType: 'road.highway',
-		elementType: 'labels.text.fill',
-		stylers: [{ color: '#f3d19c' }]
-	},
-	{
-		featureType: 'transit',
-		elementType: 'geometry',
-		stylers: [{ color: '#2f3948' }]
-	},
-	{
-		featureType: 'transit.station',
-		elementType: 'labels.text.fill',
-		stylers: [{ color: '#d59563' }]
-	},
-	{
-		featureType: 'water',
-		elementType: 'geometry',
-		stylers: [{ color: '#17263c' }]
-	},
-	{
-		featureType: 'water',
-		elementType: 'labels.text.fill',
-		stylers: [{ color: '#515c6d' }]
-	},
-	{
-		featureType: 'water',
-		elementType: 'labels.text.stroke',
-		stylers: [{ color: '#17263c' }]
-	}
+    { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
+    { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+    { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
+    {
+        featureType: 'administrative.locality',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#d59563' }]
+    },
+    {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#d59563' }]
+    },
+    {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [{ color: '#263c3f' }]
+    },
+    {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#6b9a76' }]
+    },
+    {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [{ color: '#38414e' }]
+    },
+    {
+        featureType: 'road',
+        elementType: 'geometry.stroke',
+        stylers: [{ color: '#212a37' }]
+    },
+    {
+        featureType: 'road',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#9ca5b3' }]
+    },
+    {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{ color: '#746855' }]
+    },
+    {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [{ color: '#1f2835' }]
+    },
+    {
+        featureType: 'road.highway',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#f3d19c' }]
+    },
+    {
+        featureType: 'transit',
+        elementType: 'geometry',
+        stylers: [{ color: '#2f3948' }]
+    },
+    {
+        featureType: 'transit.station',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#d59563' }]
+    },
+    {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [{ color: '#17263c' }]
+    },
+    {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#515c6d' }]
+    },
+    {
+        featureType: 'water',
+        elementType: 'labels.text.stroke',
+        stylers: [{ color: '#17263c' }]
+    }
 ]
